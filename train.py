4ee71512f1b9da97ddad9405c838d4a977a5e6ef
Adam <49051462+xhh12381@users.noreply.github.com>
2022-07-13 13:49:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ from unet import UNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
-dir_img = '/kaggle/input/aitex-dataset/imgs/'
-dir_mask = '/kaggle/input/aitex-dataset/masks/'
+dir_img = '/kaggle/input/tiles-dataset/imgs/'
+dir_mask = '/kaggle/input/tiles-dataset/masks/'
 dir_checkpoint = Path('./checkpoints/')
 
 
@@ -147,7 +147,7 @@ def train_net(net,
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=60, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=8, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-4,
                         help='Learning rate', dest='lr')
